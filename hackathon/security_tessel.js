3925104fe6502b4fda7782e24b1b96a85a353760
curt-mitch <curtis.l.mitchell@gmail.com>
2014-07-19 12:09:12 -0700

    Testing ambient sound settings.

--- hackathon/security_tessel.js
+++ hackathon/security_tessel.js
@@ -1,5 +1,14 @@
 // script for Tessel hackathon at Hack Reactor 07-19-2014
 
 var tessel = require('tessel');
-var camera = require('camera-vc-0706').use(tessel.port['A']);
+var camera = require('camera-vc0706').use(tessel.port['A']);
 var ambient = require('ambient-attx4').use(tessel.port['B']);
+
+ambient.on('ready', function(){
+	ambient.setSoundTrigger(0.15);
+
+	ambient.on('sound-trigger', function(data){
+		console.log('Heard a loud sound!');
+		console.log(data);
+	});
+});
